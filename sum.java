6a53573737ebfb7a7e62c35bcaa3115cf5bb423e
hun-t-er <manishkrsah88@gmail.com>
2022-05-31 16:59:24 +0530

    added

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String[] args) {
+        System.out.println(sum1(10,20));
+    }
+    static int sum1(int a, int b){
+        int sum2=a+b;
+        return sum2; 
+    }
+    
+}
